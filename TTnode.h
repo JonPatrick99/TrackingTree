73ad85319253b070f087aad86d589f515da7ba5b
JonPatrick99 <43153901+JonPatrick99@users.noreply.github.com>
2019-05-31 17:37:00 -0600

    Create TTnode.h

---
+++ TTnode.h
@@ -0,0 +1,60 @@
+// TTnode.h
+// Jon Caldwell
+// Project 2: Tracking Tree
+
+
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+using std::hash;
+class TTnode
+{
+public:
+
+	TTnode(string raw);						
+	string getID();							
+	string getParentID();
+	string getRawEvent();
+	string getRHash();
+	string getLHash();
+	vector<string> getRHisth();
+	vector<string> getLHisth();
+	TTnode* getLeft();
+	TTnode* getRight();
+	hash<string> getHashTb();
+
+	void setLeft(TTnode* in);				
+	void setRight(TTnode* in);
+	void setID(string in);
+	void setParentID(string in);
+	void setRawEvent(string in);
+	void setRhash(string in);
+	void setLhash(string in);
+	void addRhisth(string in);
+	void addLhisth(string in);
+
+	/*
+	void display();							
+	void displayLeft(TTnode* subtree, std::string prefix);
+	void displayRight(TTnode* subtree, std::string prefix);
+
+	void displayRec();						
+	void displayChildRec(TTnode* subtree);
+
+	*/
+
+private:
+
+	string id;
+	string parentID;
+	string rawEvent;
+	string rHash;
+	string lHash;
+	vector<string> rHisth;
+	vector<string> lHisth;
+	TTnode* leftChild;
+	TTnode* rightChild;
+	hash<string> hashTb;
+};
